3681b101b21e9c7ac657beeeadc37b966d2ba11a
Faishal Pasa <faishal.pasa@bareksa.com>
2023-05-13 18:07:00 +0700

    feature: add message

--- app/Http/Controllers/ApiControllers/GameController.php
+++ app/Http/Controllers/ApiControllers/GameController.php
@@ -203,72 +203,72 @@ class GameController extends Controller
     //   ], 404);
     // }
 
-    try {
-      $periods = DB::table('periods')
-        ->where('end_at', '<', date('Y-m-d'))
-        ->orderBy('start_at', 'desc')
-        ->get();
+    // try {
+    $periods = DB::table('periods')
+      ->where('end_at', '<', date('Y-m-d'))
+      ->orderBy('start_at', 'desc')
+      ->get();
 
-      $first_period_id = $periods[0]->id ?? null;
-      $period_id = $query_period ?? $first_period_id;
+    $first_period_id = $periods[0]->id ?? null;
+    $period_id = $query_period ?? $first_period_id;
 
-      $selected_periods = DB::table('periods')
-        ->where('id', $period_id)
-        ->first();
+    $selected_periods = DB::table('periods')
+      ->where('id', $period_id)
+      ->first();
 
-      $prizes = DB::table('prizes')
-        ->where('period_id', $selected_periods->id)
-        ->get();
+    $prizes = DB::table('prizes')
+      ->where('period_id', $selected_periods->id)
+      ->get();
 
-      $prize_count = count($prizes);
+    $prize_count = count($prizes);
 
-      $s_date = date('Y-m-d 00:00:00', strtotime(date($selected_periods->start_at ?? 'Y-m-d')));
-      $e_date = date('Y-m-d 23:59:59', strtotime(date($selected_periods->end_at ?? 'Y-m-d')));
+    $s_date = date('Y-m-d 00:00:00', strtotime(date($selected_periods->start_at ?? 'Y-m-d')));
+    $e_date = date('Y-m-d 23:59:59', strtotime(date($selected_periods->end_at ?? 'Y-m-d')));
 
-      $players = DB::table('player_logs')
-        ->leftJoin('players', 'player_logs.player_id', 'players.id')
-        ->leftJoin('levels', 'player_logs.level_id', 'levels.id')
-        ->select(
-          'players.id as player_id',
-          'players.name as player_name',
-          'players.msisdn as player_msisdn',
-          'players.email as player_email',
-          DB::raw('SUM(player_logs.score) as total_score'),
-          DB::raw('SUM(player_logs.time) as total_time'),
-          DB::raw('MAX(levels.name) as max_level'),
-        )
-        ->where('player_logs.created_at', '>', $s_date)
-        ->where('player_logs.created_at', '<', $e_date)
-        ->whereNotNull('players.id')
-        ->groupBy('players.id')
-        ->orderBy('total_score', 'desc')
-        ->limit($prize_count)
-        ->get();
+    $players = DB::table('player_logs')
+      ->leftJoin('players', 'player_logs.player_id', 'players.id')
+      ->leftJoin('levels', 'player_logs.level_id', 'levels.id')
+      ->select(
+        'players.id as player_id',
+        'players.name as player_name',
+        'players.msisdn as player_msisdn',
+        'players.email as player_email',
+        DB::raw('SUM(player_logs.score) as total_score'),
+        DB::raw('SUM(player_logs.time) as total_time'),
+        DB::raw('MAX(levels.name) as max_level'),
+      )
+      ->where('player_logs.created_at', '>', $s_date)
+      ->where('player_logs.created_at', '<', $e_date)
+      ->whereNotNull('players.id')
+      ->groupBy('players.id')
+      ->orderBy('total_score', 'desc')
+      ->limit($prize_count)
+      ->get();
 
-      $winners = [];
+    $winners = [];
 
-      foreach ($prizes as $idx => $prize) {
-        $winners[$idx] = [
-          'player_id' => $players[$idx]->player_id,
-          'player_name' => $players[$idx]->player_name,
-          'player_msisdn' => $players[$idx]->player_msisdn,
-          'total_score' => $players[$idx]->total_score,
-          'prize_name' => $prize->name,
-        ];
-      }
-
-      return Response::json([
-        'success' => true,
-        'code' => 200,
-        'data' => $winners
-      ], 200);
-    } catch (\Throwable $e) {
-      return Response::json([
-        'success' => false,
-        'code' => 500,
-        'message' => 'Terjadi kesalahan ketika memproses data.'
-      ], 500);
+    foreach ($prizes as $idx => $prize) {
+      $winners[$idx] = [
+        'player_id' => $players[$idx]->player_id,
+        'player_name' => $players[$idx]->player_name,
+        'player_msisdn' => $players[$idx]->player_msisdn,
+        'total_score' => $players[$idx]->total_score,
+        'prize_name' => $prize->name,
+      ];
     }
+
+    return Response::json([
+      'success' => true,
+      'code' => 200,
+      'data' => $winners
+    ], 200);
+    // } catch (\Throwable $e) {
+    //   return Response::json([
+    //     'success' => false,
+    //     'code' => 500,
+    //     'message' => 'Terjadi kesalahan ketika memproses data.'
+    //   ], 500);
+    // }
   }
 
   public function continue_play(Request $request)
